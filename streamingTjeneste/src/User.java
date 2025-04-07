49994da06def16cfc2ca938b1e3149fc17cf7717
olivermjmj <olivermjmj@outlook.dk>
2025-04-07 10:35:50 +0200

    Committing the feature of creating a new account and being able to login.

--- streamingTjeneste/src/User.java
+++ streamingTjeneste/src/User.java
@@ -1,4 +1,7 @@
 
+import util.FileIO;
+import util.TextUI;
+
 import java.io.*;
 
 import java.security.NoSuchAlgorithmException;
@@ -10,101 +13,60 @@ import javax.crypto.spec.PBEKeySpec;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-
 public class User {
 
-    private static final SecureRandom RANDOM = new SecureRandom();
-    private static final int ITERATIONS = 10000;
-    private static final int KEY_LENGTH = 256;
-
-    private static final String USERDATA_DIR = "userdata";
-
-    private static byte[] hashPasswd(char[] passwd, byte[] salt) {
-        PBEKeySpec spec = new PBEKeySpec(passwd, salt, ITERATIONS, KEY_LENGTH);
-        Arrays.fill(passwd, Character.MIN_VALUE);
-        try {
-            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            return skf.generateSecret(spec).getEncoded();
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            throw new AssertionError("Error hashing password: " + e.getMessage(), e);
-        } finally {
-            spec.clearPassword();
-        }
-    }
-
-    public static User login(String name, String passwd) {
-        byte[] hashbytes;
-        byte[] hash;
-        byte[] salt;
-        DataInputStream in;
-
-        salt = new byte[16];
-        hash = new byte[32];
-
-        File f = new File(USERDATA_DIR+"/"+name+".udf");
-        try {
-            in = new DataInputStream(new BufferedInputStream(
-                    new FileInputStream(f)));
-
-            int read = in.read(hash, 0, hash.length);
-            read += in.read(salt, 0, salt.length);
-            if (read <= 0)
-                throw new IOException("read failed");
-
-        } catch (FileNotFoundException e) {
-            System.err.println(e.getMessage());
-            return null;
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            return null;
-        }
-
-        hashbytes = hashPasswd(passwd.toCharArray(), salt);
-
-        if (Arrays.equals(hash, hashbytes)) {
-            return new User(name);
-        }
-        return null;
-    }
-
-    public static boolean register(String name, String passwd) {
-        byte[] hashbytes;
-        byte[] salt;
-        DataOutputStream out;
-
-        salt = new byte[16];
-        RANDOM.nextBytes(salt);
-        hashbytes = hashPasswd(passwd.toCharArray(), salt);
-
-        new File(USERDATA_DIR).mkdir();
-        File f = new File(USERDATA_DIR+"/"+name+".udf");
-        if (f.exists())
-            return false;
-        try {
-            out = new DataOutputStream(new BufferedOutputStream(
-                    new FileOutputStream(f)));
-
-            out.write(hashbytes);
-            out.write(salt);
-            out.close();
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found");
-            return false;
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            return false;
-        }
-        return true;
-    }
-
     public String name;
-    ArrayList<Integer> watchhistory;
-    ArrayList<Integer> watchlater;
+
+    static TextUI UI = new TextUI();
+    static FileIO IO = new FileIO();
 
     public User(String name) {
         this.name = name;
     }
 
-    public void save() {
+    public static User login(String name, String passwd) {
+
+        String userLogin = name + "," + passwd;
+        ArrayList<String> users = IO.loadUserData("data/userData.csv");
+
+        for (String user : users) {
+            if (user.trim().equals(userLogin)) {
+
+                return new User(name);
+            }
+        }
+
+        return null;
     }
+
+    public static boolean register(String name, String passwd) {
+
+        IO.saveUserData("data/userData.csv", name, passwd);
+        return true;
+    }
+
+
+/*
+    public static boolean userLoggingIn() {
+
+        String userLogin = username + "," + password;
+        ArrayList<String> users = IO.loadUserData("data/userData.csv");
+
+        for (String user : users) {
+            if (user.trim().equals(userLogin)) {
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+ */
 }
+
+
+
+
+
+
