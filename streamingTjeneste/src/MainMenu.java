aaf81556f0d701382e41027af3ce06b2123f421b
olivermjmj <olivermjmj@outlook.dk>
2025-04-10 09:24:59 +0200

    test

--- streamingTjeneste/src/MainMenu.java
+++ streamingTjeneste/src/MainMenu.java
@@ -29,7 +29,7 @@ public class MainMenu extends JPanel implements ActionListener, ListSelectionLis
     private final JComboBox<String> cb;
     private JComboBox<String> ratingCb;
     private JList<Content> list;
-    private ContentListModel<Content> listModelContent;
+    private ContentListModel<Content> listModelContent = new ContentListModel<>();
     List<Movies> moviesList;
 
     MainMenu() {
