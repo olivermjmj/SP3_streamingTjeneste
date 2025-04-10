d2e1fab2f63c7b39e7f7eae3c43f8d62c11b4df1
olivermjmj <olivermjmj@outlook.dk>
2025-04-10 21:46:33 +0200

    Final product

--- streamingTjeneste/src/main/ChooseToWatchLater.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class ChooseToWatchLater {
-
-    public String addShow() {
-
-        return null;
-    }
-}
--- streamingTjeneste/src/main/Play.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Play {
-
-    public void watchShow() {
-
-
-    }
-}
